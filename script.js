e91ad57acf2f9b41679327ff135f8860ad264719
marwane <marw0619@stud.kea.dk>
2021-04-27 11:48:35 +0200

    changing pagesjs

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 import  "./style.scss";
+loadJSON();
+document.querySelector(".buttonNext").addEventListener("click",showPt2);
+
+function showPt2 () {
+    console.log("haha");
+    document.querySelector(".userInfo").classList.add("hide");
+    document.querySelector("#gameInfo").classList.remove("hide");
+    document.querySelector("#previousB").addEventListener("click",backPt1);
+
+}
+
+function backPt1 () {
+    document.querySelector(".userInfo").classList.remove("hide");
+    document.querySelector("#gameInfo").classList.add("hide");
+    document.querySelector(".buttonNext").addEventListener("click",showPt2);
+}
+
+function loadJSON() {
+    console.log("loadJSON");
+    fetch("./json/data.json")
+    .then(r => r.json())
+    .then (jsonData => {
+        // loaded --> prepare objects
+        console.log(jsonData);
+        prepLists(jsonData);
+    });
+}
+
+function prepLists(jsonData) {
+    jsonData.areas.forEach(jsonObject => {
+        var newOP = document.createElement("option");
+        newOP.textContent =jsonObject;
+        document.querySelector(".gameType select").appendChild(newOP);
+    });
+    jsonData.games.forEach(jsonObject => {
+        var newOP = document.createElement("option");
+        newOP.textContent =jsonObject;
+        document.querySelector(".gameChoice select").appendChild(newOP);
+    });
+    jsonData.types.forEach(jsonObject => {
+        var newOP = document.createElement("option");
+        newOP.textContent =jsonObject;
+        document.querySelector(".gameImprove select").appendChild(newOP);
+    });
+}
+
 
